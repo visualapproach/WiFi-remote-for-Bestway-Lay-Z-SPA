656898178252a564e85df67f01a7dc6f01d649a7
visualapproach <info@visualapproach.se>
2020-09-13 18:34:21 +0200

    Update a_globals.h

--- Code/ESP12E_BWC_HWchecker_v0.2/a_globals.h
+++ Code/ESP12E_BWC_HWchecker_v0.2/a_globals.h
@@ -4,7 +4,7 @@
 #include <ArduinoOTA.h>
 #include <ESP8266WebServer.h>
 //#include <ESP8266mDNS.h>
-#include <FS.h>
+#include <LittleFS.h>
 #include <WebSocketsServer.h>
 //used by config portal
 #include <DNSServer.h>
