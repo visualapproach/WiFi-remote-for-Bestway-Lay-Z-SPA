50eec20e2dd9ff0d48e4fa3109b409e0b72a957a
torei <t@kaytee.one>
2022-01-04 21:00:23 +0100

    add function to toggle password fields to plain text

--- Code/6-wire-version/data/function.js
+++ Code/6-wire-version/data/function.js
@@ -7,3 +7,12 @@ function topNav() {
         x.className = "topnav";
     }
 }
+
+function togglePlainText(id) {
+    var x = document.getElementById(id);
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
