232e013a14d68fccb47837049dae29bfde0e0c73
torei <t@kaytee.one>
2022-01-17 12:42:25 +0100

    update 4-wire version (with 6-wire changes)

---
+++ Code/4-wire-version/data/function.js
@@ -0,0 +1,27 @@
+
+function topNav() {
+	var x = document.getElementById("topnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
+
+function togglePlainText(id) {
+	var x = document.getElementById(id);
+	if (x.type === "password") {
+		x.type = "text";
+	} else {
+		x.type = "password";
+	}
+}
+
+function validatePassword(id) {
+	var x = document.getElementById(id);
+	if (x.value == "<enter password>") {
+		alert("Please enter a password to continue.");
+		return false;
+	}
+	return true;
+}
