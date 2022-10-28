ce3b932bf1c96e463c28fe74da406e4b00fd65fe
Visualapproach <info@visualapproach.se>
2022-10-28 22:48:21 +0200

    6w: bugfix

--- Code/6-wire-version/lib/BWC/BWC_common.cpp
+++ Code/6-wire-version/lib/BWC/BWC_common.cpp
@@ -264,7 +264,7 @@ void BWC::_calcVirtualTemp()
   float newvt = _virtualTempFix + netRisePerHour * elapsed_hours;
 
   // clamp VT to +/- 1 from measured temperature if pump is running
-  if(_cio.states[PUMPSTATE] && (_cio.state_age[PUMPSTATE] > 1*60000))
+  if(_cio.states[PUMPSTATE] && (_cio.state_age[PUMPSTATE] > 5*60000))
   {
     float tempInC = _cio.states[TEMPERATURE];
     float limit = 0.99;
