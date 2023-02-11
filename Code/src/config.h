978f3cd8f3941fa7f21aa7f6b86b7416b15ee3d7
Visualapproach <info@visualapproach.se>
2023-02-11 09:58:16 +0100

    firmware date update

--- Code/src/config.h
+++ Code/src/config.h
@@ -5,7 +5,7 @@
 #include <WiFi.h>
 #endif
 #define DEVICE_NAME "layzspa"
-#define FW_VERSION "2023-02-05-alpha"
+#define FW_VERSION "2023-02-11-alpha"
 #define HA_PREFIX "homeassistant"
 #define PROM_NAMESPACE "layzspa"
 
