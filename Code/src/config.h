2ebac530218e0ba5f7cea6b2cbe33668c4e59417
Visualapproach <info@visualapproach.se>
2023-03-05 00:53:24 +0100

    Feature: Update FW via web UI

--- Code/src/config.h
+++ Code/src/config.h
@@ -5,7 +5,7 @@
 #include <WiFi.h>
 #endif
 #define DEVICE_NAME "layzspa"
-#define FW_VERSION "2023-03-05-0029-beta"
+#define FW_VERSION "2023-03-05-0041-beta"
 #define HA_PREFIX "homeassistant"
 #define PROM_NAMESPACE "layzspa"
 
