8e4ef1a411574388f14f99097f5044c10988aa6c
Visualapproach <info@visualapproach.se>
2023-03-13 20:00:52 +0100

    update fw version

--- Code/src/config.h
+++ Code/src/config.h
@@ -5,7 +5,7 @@
 #include <WiFi.h>
 #endif
 #define DEVICE_NAME "layzspa"
-#define FW_VERSION "2023-03-12-1600"
+#define FW_VERSION "2023-03-13-2000"
 
 // String FW_VERSION = String(__DATE__) + "-" + __TIME__ + "-beta";
 #define HA_PREFIX "homeassistant"
