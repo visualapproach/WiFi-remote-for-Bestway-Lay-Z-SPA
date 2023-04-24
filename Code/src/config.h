a2356ed37bd40c017e99d1051f969d580a7560c1
Visualapproach <info@visualapproach.se>
2023-04-24 19:36:14 +0200

    Use several spas with different names in HA And enable jets switch

--- Code/src/config.h
+++ Code/src/config.h
@@ -5,7 +5,7 @@
 #include <WiFi.h>
 #endif
 #define DEVICE_NAME "layzspa"
-#define FW_VERSION "2023-04-23-1052"
+#define FW_VERSION "2023-04-24-1854"
 
 // String FW_VERSION = String(__DATE__) + "-" + __TIME__ + "-beta";
 #define HA_PREFIX "homeassistant"
