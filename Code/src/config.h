cee618951d49b64c472ce7ec2b4b2aedca7bfd66
Visualapproach <info@visualapproach.se>
2023-04-24 21:31:30 +0200

    edit remark

--- Code/src/config.h
+++ Code/src/config.h
@@ -7,7 +7,6 @@
 #define DEVICE_NAME "layzspa"
 #define FW_VERSION "2023-04-24-1854"
 
-// String FW_VERSION = String(__DATE__) + "-" + __TIME__ + "-beta";
 #define HA_PREFIX "homeassistant"
 #define PROM_NAMESPACE "layzspa"
 
