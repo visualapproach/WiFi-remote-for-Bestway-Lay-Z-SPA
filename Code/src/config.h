6cbd3eaaa5c6baabd345ef5c9c13817b5c23a868
Visualapproach <info@visualapproach.se>
2023-05-13 15:52:45 +0200

    update fw

--- Code/src/config.h
+++ Code/src/config.h
@@ -5,7 +5,7 @@
 #include <WiFi.h>
 #endif
 #define DEVICE_NAME "layzspa"
-#define FW_VERSION "2023-05-10-1137"
+#define FW_VERSION "2023-05-13-1551"
 
 #define HA_PREFIX "homeassistant"
 #define PROM_NAMESPACE "layzspa"
