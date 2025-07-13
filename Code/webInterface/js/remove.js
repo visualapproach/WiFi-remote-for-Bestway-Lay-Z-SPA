38a26d67e873d11330f433263591bc6e19e07608
sjsone <simon@sjs.one>
2025-07-13 17:41:55 +0200

    chore: restructuring and template engine
    refs: #869

---
+++ Code/webInterface/js/remove.js
@@ -0,0 +1,16 @@
+function removeFile() {
+    var filename = document.getElementById('fileToRemove').value
+    if (!filename) {
+        alert(getTranslation('alert_enter_filename'))
+        return
+    }
+    var req = new XMLHttpRequest()
+    req.open('POST', '/removefile/')
+    req.send(JSON.stringify({ file: filename }))
+    req.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            alert(getTranslation('alert_file_removed'))
+        }
+    }
+    document.getElementById('fileToRemove').value = ''
+}
